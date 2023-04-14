3d3d2429b89fb5e36963ecd29071e09b5d3c6f22
KreenaShah <102236893+KreenaShah@users.noreply.github.com>
2023-04-14 17:53:49 +0530

    PRs scrapped

--- PullRequests.py
+++ PullRequests.py
@@ -1,10 +1,8 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 import time
 
-# Replace the following variables with the URL of the website
-# and the path to the chromedriver executable on your machine
+# Replace the following variables with the URL of the website and the path to the chromedriver executable on your machine
 website_url = "https://github.com/facebook/react/pulls"
 chromedriver_path = "/path/to/chromedriver"
 
@@ -14,8 +12,7 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--headless")
 
 # Create a new Chrome WebDriver instance
-browser = webdriver.Chrome(
-    executable_path=chromedriver_path, options=chrome_options)
+browser = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
 
 # Navigate to the website
 browser.get(website_url)
@@ -27,6 +24,21 @@ browser.get(website_url)
 #     pulls_single_page.append(pull)
 # print(pulls_single_page)
 
+# --------------------------------------------------------------------------------------------
+
+button = browser.find_element(By.CLASS_NAME, 'next_page') 
+# Move up to the parent element
+parent_element = button.find_element(By.XPATH ,'..')
+# Get the text content of the parent element
+text_before_button = parent_element.text
+# Extract the number before "Next" using string manipulation
+number_before_next = text_before_button.split(' ')[-2]
+# Print the number before "Next"
+# print("Number before 'Next':", number_before_next)
+# print(type(number_before_next))
+last_page_no = int(number_before_next)
+# print(type(last_page_no))
+
 pulls=[]
 
 elements = browser.find_elements(By.XPATH, "//a[@class='Link--primary v-align-middle no-underline h4 js-navigation-open markdown-title']")
@@ -35,7 +47,7 @@ for element in elements:
     # print(pull)
     pulls.append(pull)
 
-for i in range(0,12):
+for i in range(0,last_page_no):
     button = browser.find_element(By.CLASS_NAME, 'next_page')  # Replace with the appropriate XPath for the next paginated button
     button.click()
 
@@ -51,5 +63,8 @@ for i in range(0,12):
 
 print(pulls)
 print(len(pulls))
+
+# --------------------------------------------------------------------------------------------
+
 # Close the browser
 browser.quit()
